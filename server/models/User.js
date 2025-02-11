bf1d1d9c271e3b48ef48979b81e467caf7929356
MollyMyBean <dtru924@gmail.com>
2025-02-11 13:28:57 -0500

    Update User.js

--- server/models/User.js
+++ server/models/User.js
@@ -25,13 +25,21 @@ const userSchema = new Schema({
   profilePic: {
     type: String
   },
-
-  /* NEW field => admin’s banner photo, used in suggested creators. */
+  // NEW field: Admin’s feed banner (for use on ProfilePage, etc.)
+  profileBanner: {
+    type: String,
+    default: ''
+  },
+  // NEW field: Admin’s banner photo (for suggested creators)
   bannerPic: {
     type: String,
     default: ''
   },
-
+  // NEW field: Banner used on the Profile Preview tab (available for all users)
+  previewBanner: {
+    type: String,
+    default: ''
+  },
   purchasedBundles: [
     {
       bundleId: { type: mongoose.Schema.Types.ObjectId },
@@ -41,46 +49,40 @@ const userSchema = new Schema({
       purchasedAt: Date
     }
   ],
-
   follows: [
-    { type: Schema.Types.ObjectId, ref: 'User' }
+    { type: mongoose.Schema.Types.ObjectId, ref: 'User' }
   ],
   subscriptions: [
     {
-      adminId: { type: Schema.Types.ObjectId, ref: 'User' },
+      adminId: { type: mongoose.Schema.Types.ObjectId, ref: 'User' },
       validUntil: { type: Date }
     }
   ],
-
   adminBundles: [
     {
       _id: { type: mongoose.Schema.Types.ObjectId, default: () => new mongoose.Types.ObjectId() },
       title: String,
       price: Number,
       description: String,
-
-      // Added for cover images:
+      // Field for bundle cover image
       coverUrl: {
         type: String,
         default: ''
       }
     }
   ],
-
   paymentMethod: { type: String, default: '' },
   cardNumber: { type: String },
   cardExp: { type: String },
   cardCVC: { type: String },
   paypalEmail: { type: String },
   cryptoAddress: { type: String },
-
   likedAdmins: [
-    { type: Schema.Types.ObjectId, ref: 'User' }
+    { type: mongoose.Schema.Types.ObjectId, ref: 'User' }
   ],
   likedUsers: [
-    { type: Schema.Types.ObjectId, ref: 'User' }
+    { type: mongoose.Schema.Types.ObjectId, ref: 'User' }
   ],
-
   // Additional profile/bio fields
   bio: { type: String, default: '' },
   interests: { type: String, default: '' },
